5d9bf1728084dfd2700b1f337fcf6789c38f4ac2
Dmitry-Bryliuk <dmitry.bryliuk@evote.com>
2018-06-08 20:18:25 +0300

    try more firebase-mock

--- functions/test/test.frebasemock.js
+++ functions/test/test.frebasemock.js
@@ -8,7 +8,7 @@ firebasemock.MockFirebase.override = function () {
 
 firebasemock.MockFirebase.override();
 
-var db = firebase.firestore();
+/*var db = firebase.firestore();
 
 var docRef = db.collection('users').doc('alovelace2');
 
@@ -33,7 +33,6 @@ console.log("setAlan");
 console.log(setAlan);
 setAlan.then(r => console.log(r)).catch(e => { });
 
-
 firebase.firestore().collection('users').get()
     .then((snapshot) => {
         console.log("snapshot:");
@@ -45,3 +44,77 @@ firebase.firestore().collection('users').get()
     .catch((err) => {
         console.log('Error getting documents', err);
     });
+
+
+firebase.firestore().collection('people').add({
+    first: 'Michael'
+});
+firebase.firestore().collection('people').add({
+    first: 'Ben'
+});
+
+firebase.firestore().collection('people').get()
+    .then(snaps => {
+        snaps.forEach(doc => {
+            console.log(doc.data());
+        });
+        return;
+    })
+    .catch((err) => {
+        console.log('Error getting documents', err);
+    });*/
+
+//firebase.firestore().collection('people').flush();
+
+
+var collection;
+var people = {
+    collection: function () {
+        if (!collection) collection = firebase.firestore().collection('people');
+        return collection;
+    },
+    greet: function (person) {
+        console.log('hi ' + person.first);
+    },
+    process: function () {
+        console.log("process people...");
+        people.collection().get().then(function (snaps) {
+            snaps.forEach(function (doc) {
+                people.greet(doc.data());
+            });
+        });
+    }
+};
+
+var greeted = [];
+people.greet = function (person) {
+    greeted.push(person);
+};
+people.collection().add({
+    first: 'Michael'
+});
+people.collection().add({
+    first: 'Ben'
+});
+
+people.process();
+console.log(greeted);
+
+people.collection().flush();
+console.log(greeted);
+
+//console.assert(greeted.length === 2, '2 people greeted');
+//console.assert(greeted[0].first === 'Michael', 'Michael greeted');
+//console.assert(greeted[1].first === 'Ben', 'Ben greeted');
+
+people.create = function (first) {
+    return people.collection().add({
+        first: first
+    });
+};
+
+var newPersonRef = people.create('James');
+newPersonRef.then(function (doc) {
+    console.assert(doc.get('first') === 'James', 'James was created');
+});
+people.collection().flush();
