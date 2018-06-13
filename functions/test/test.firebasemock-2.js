5a828a290503238510ab5ae125cbdff67407c858
Dmitry-Bryliuk <dmitry.bryliuk@evote.com>
2018-06-13 20:23:26 +0300

    success firebase mock

--- functions/test/test.firebasemock-2.js
+++ functions/test/test.firebasemock-2.js
@@ -1,6 +1,19 @@
 var firebase_mock = require('firebase-mock');
 var firestore = new firebase_mock.MockFirestore();
 
+function listSnapshot(snapshot, title) {
+    !title || console.log(title);
+    !snapshot || snapshot.forEach(doc => console.log(doc.id, '=>', doc.data()));
+    console.log("<");
+}
+
+firestore.autoFlush();
+
+firestore.collection("users").get().then(r => listSnapshot(r, "before:"));
+
 firestore.collection("users").doc("aaa").set({ name: "aaa" }).then(r => console.log(r));
-firestore.collection("users").add({ name: "bbb" }).then(r => console.log(r));
-firestore.collection("users").get().then(r => console.log(r));
+firestore.collection("users").add({ name: "bbb" }).then(r => console.log(r.id, '=>', r.data));
+
+firestore.collection("users").get().then(r => listSnapshot(r, "after:"));
+
+//firestore.flush();
