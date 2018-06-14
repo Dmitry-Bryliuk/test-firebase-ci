bd9508f28eb314566c2d02ff1a1f15cfcca4abe9
Dmitry-Bryliuk <dmitry.bryliuk@evote.com>
2018-06-14 17:59:07 +0300

    fix output

--- functions/test/test.firebasemock-2.js
+++ functions/test/test.firebasemock-2.js
@@ -11,8 +11,8 @@ firestore.autoFlush();
 
 firestore.collection("users").get().then(r => listSnapshot(r, "before:"));
 
-firestore.collection("users").doc("aaa").set({ name: "aaa" }).then(r => console.log(r));
-firestore.collection("users").add({ name: "bbb" }).then(r => console.log(r.id, '=>', r.data));
+firestore.collection("users").doc("aaa").set({ name: "aaa" }).then(r => console.log("set:", r));
+firestore.collection("users").add({ name: "bbb" }).then(r => console.log("add:", r.id, '=>', r.data));
 
 firestore.collection("users").get().then(r => listSnapshot(r, "after:"));
 
