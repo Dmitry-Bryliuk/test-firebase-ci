6f8547955da0785f9ebd9417aa5d6202bc589179
Dmitry-Bryliuk <dmitry.bryliuk@evote.com>
2018-06-15 17:11:08 +0300

    try functions mock

--- functions/test/test.firebasemock-2.js
+++ functions/test/test.firebasemock-2.js
@@ -1,5 +1,12 @@
 var firebase_mock = require('firebase-mock');
 var firestore = new firebase_mock.MockFirestore();
+//var database = new firebase_mock.MockFirebase();
+var functions = require('firebase-functions');
+
+functions.firestore.document('users/{uid}').onCreate(r => console.log("onCreate:", r))
+//functions.database.ref('/messages/{pushId}/original').onCreate((snapshot, context) => { console.log('Uppercasing', context.params.pushId, original); })
+
+//database.ref('/messages').push({ original: "original" }).then((snapshot) => { console.log(snapshot); });
 
 function listSnapshot(snapshot, title) {
     !title || console.log(title);
